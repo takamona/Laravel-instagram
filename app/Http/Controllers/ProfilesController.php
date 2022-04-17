23fba49c0a5476807f7a8f5f6495ccede295464c
TakahisaKobayashi <takavyd2720ray@docomo.ne.jp>
2022-04-18 00:29:44 +0900

    プロフィール編集完了

--- app/Http/Controllers/ProfilesController.php
+++ app/Http/Controllers/ProfilesController.php
@@ -8,23 +8,12 @@ use Illuminate\Http\Request;
 class ProfilesController extends Controller
 {
     /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function index()
-    {
-        //
-    }
-
-    /**
      * Show the form for creating a new resource.
      *
      * @return \Illuminate\Http\Response
      */
     public function create()
     {
-        
          // 空のプロフィールインスタンス作成
         $profile = new Profile();
         // view の呼び出し
@@ -81,6 +70,7 @@ class ProfilesController extends Controller
         return redirect('/top')->with('flash_message', 'プロフィールを作成しました');
 
     }
+    
     /**
      * Display the specified resource.
      *
@@ -101,6 +91,14 @@ class ProfilesController extends Controller
     public function edit(Profile $profile)
     {
         
+         // ログインしている自分のプロフィールの場合
+        if($profile->user_id === \Auth::id()){
+            // view の呼び出し
+            return view('profiles.edit', compact('profile'));
+        }else{
+            return redirect('/top');
+        }
+        
     }
 
     /**
@@ -112,8 +110,57 @@ class ProfilesController extends Controller
      */
     public function update(Request $request, Profile $profile)
     {
-        //
+        
+         // ログインしている自分のプロフィールの場合
+        if($profile->user_id === \Auth::id()){
+            // validation
+            //for image ref) https://qiita.com/maejima_f/items/7691aa9385970ba7e3ed
+            $this->validate($request, [
+                'nickname' => 'required',
+                'gender' => 'required',
+                'introduction' => 'required',
+                'image' => [
+                    'file',
+                    'mimes:jpeg,jpg,png'
+                ]
+            ]);
+            
+            // 入力情報の取得
+            $nickname = $request->input('nickname');
+            $gender = $request->input('gender');
+            $introduction = $request->input('introduction');
+            $file =  $request->image;
+            
+            // 画像ファイルのアップロード
+            // https://qiita.com/ryo-program/items/35bbe8fc3c5da1993366
+            if($file){
+                // 現在時刻ともともとのファイル名を組み合わせてランダムなファイル名作成
+                $image = time() . $file->getClientOriginalName();
+                // アップロードするフォルダ名取得
+                $target_path = public_path('uploads/');
+                // アップロード処理
+                $file->move($target_path, $image);
+            }else{
+                // 画像を選択していなければ、画像ファイルは元の名前のまま
+                $image = $profile->image;
+            }
+        
+    // 入力情報をもとにインスタンスのプロパティ変更
+            $profile->nickname = $nickname;
+            $profile->gender = $gender;
+            $profile->introduction = $introduction;
+            $profile->image = $image;
+            
+            // データベース更新
+            $profile->save();
+    
+            // トップページへリダイレクト
+            return redirect('/top')->with('flash_message', 'プロフィールを変更しました。');
+        }else{
+            return redirect('/top');
+        }
     }
+    
 
     /**
      * Remove the specified resource from storage.
