7d2e320ef8803e146d81e6efd0bcd03f9c1c7c10
TakahisaKobayashi <takavyd2720ray@docomo.ne.jp>
2022-04-20 16:28:16 +0900

    いいねランキング表示完成

--- routes/web.php
+++ routes/web.php
@@ -51,6 +51,9 @@ Route::group(['middleware' => ['auth']], function () {
     // タイムライン関係
     Route::get('timelines', 'UsersController@timelines')->name('users.timelines');
     
+    // いいねランキング
+    Route::get('rankings', 'PostsController@rankings')->name('posts.rankings');
+    
     // ネスト
     Route::group(['prefix' => 'users/{id}'], function () {
         // いいねした投稿一覧
@@ -72,3 +75,4 @@ Route::group(['middleware' => ['auth']], function () {
     });
 
 });
+
