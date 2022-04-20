8a52162afbbfdf423c8cbfe024b051b7359e256a
TakahisaKobayashi <takavyd2720ray@docomo.ne.jp>
2022-04-20 16:33:23 +0900

    キーワード検索完成

--- resources/views/top.blade.php
+++ resources/views/top.blade.php
@@ -1,55 +1,57 @@
 @extends('layouts.app')
-@section('title', $user->name . 'さんのマイページ')
+@section('title',  '投稿一覧')
 @section('content')
+    @if (isset($flash_message))
+      <p class="alert alert-success" role="alert">{{ $flash_message }}</p>
+    @endif
     <div class="text-center">
-        <h1>{{ $user->name }} さんのマイページ</h1>
-    </div>
-    @if($profile)
-    <div class="row mt-5">
-        <div class="offset-sm-2 col-sm-3">
-            <img src="{{ asset('uploads')}}/{{ $profile->image }}" alt="no image" class="image_icon">
-        </div>
-        <div class="offset-sm-1 col-sm-3 pt-3">
-            <p>ニックネーム / {{ $profile->nickname }}</p>
-            <p>性別 / {{ $profile->gender === 'man' ? '男性' : '女性' }}</p>
-            <p>自己紹介 / {{ $profile->introduction }}</p>
-            <p>{!! link_to_route('users.favorites', 'お気に入り投稿一覧', ['id' => $user->id ],['class' => 'nav-link']) !!}</p>
-        </div>
-    </div>
-    @else
-    <div class="row mt-5">
-        <p class="col-sm-12 text-center">プロフィールは未設定です</p>
-    </div>
-    @endif
+        <h1>投稿一覧</h1>
+        <div class="row">
+            <div class="col-sm-6 offset-sm-3">
     
-    @if(count($posts) !== 0)
-    <div class="text-center mt-5">
-        <h2>{{ $user->name }} さんの投稿一覧</h2>
+                {!! Form::open(['route' => ['posts.search'], 'method' => 'get']) !!}
+                    <div class="form-group">
+                        {!! Form::label('keyword', 'キーワード') !!}
+                        {!! Form::text('keyword', old('title'), ['class' => 'form-control']) !!}
+                    </div>
+                    {!! Form::submit('検索', ['class' => 'btn btn-primary btn-block']) !!}
+                {!! Form::close() !!}
+            </div>
+        </div>
+        @if($posts->total() !== 0)
+        <div class="row mt-3">
+            <p>投稿件数: {{ $posts->total() }}件</p>
+            <table class="table table-bordered table-striped">
+                <tr>
+                    <th>ID</th>
+                    <th>名前</th>
+                    <th>タイトル</th>
+                    <th>内容</th>
+                    <th>投稿日時</th>
+                </tr>
+                @foreach($posts as $post)
+                <tr>
+                    <td>{!! link_to_route('posts.show', $post->id , ['id' => $post->id ],[]) !!}</td>
+                    <td>
+                        @if($post->user->profile)
+                        <img src="{{ asset('uploads')}}/{{ $post->user->profile->image }}" alt="{{ $post->user->profile->image }}" class="avatar">
+                        @else
+                        <img src="{{ asset('images/no_image.jpg') }}" alt="アバター画像は未設定です" class="avatar">
+                        @endif
+                        {!! link_to_route('users.show', $post->user->name , ['id' => $post->user->id ],[]) !!}
+                    </td>
+                    <td>{{ $post->title }}</td>
+                    <td>{{ $post->content }}</td>
+                    <td>{{ $post->created_at }}</td>
+                </tr>
+                @endforeach
+            </table>
+            {{ $posts->links('pagination::bootstrap-4') }}
+        </div>
+        @else
+        <div class="row">
+            <h2 class="mt-3 offset-sm-3 col-sm-6 text-center">投稿はありません</h2>
+        </div>
+        @endif
     </div>
-     <div class="row mt-3">
-        <table class="table table-bordered table-striped">
-            <tr>
-                <th>ID</th>
-                <th>名前</th>
-                <th>タイトル</th>
-                <th>内容</th>
-                <th>投稿日時</th>
-            </tr>
-            @foreach($posts as $post)
-            <tr>
-                <td>{!! link_to_route('posts.show', $post->id , ['id' => $post->id ],[]) !!}</td>
-                <td>{{ $post->user->name }}</td>
-                <td>{{ $post->title }}</td>
-                <td>{{ $post->content }}</td>
-                <td>{{ $post->created_at }}</td>
-            </tr>
-            @endforeach
-        </table>
-        {{ $posts->links('pagination::bootstrap-4') }}
-    </div>
-    @else
-    <div class="row mt-5">
-        <p class="col-sm-12 text-center">{{ $user->name }} さんの投稿はまだありません</p>
-    </div>
-    @endif
 @endsection
--- routes/web.php
+++ routes/web.php
@@ -54,6 +54,9 @@ Route::group(['middleware' => ['auth']], function () {
     // いいねランキング
     Route::get('rankings', 'PostsController@rankings')->name('posts.rankings');
     
+    // キーワード検索
+    Route::get('search', 'PostsController@search')->name('posts.search');
+    
     // ネスト
     Route::group(['prefix' => 'users/{id}'], function () {
         // いいねした投稿一覧
